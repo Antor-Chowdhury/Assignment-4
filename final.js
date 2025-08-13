91d1a0ef10b4c7668f3dae11347a3269848d96cc
Antor-Chowdhury <antor1347@gmail.com>
2025-08-14 00:07:45 +0600

    add problem-4 solution in Assignment

--- final.js
+++ final.js
@@ -39,3 +39,21 @@ function bestTeam(player1, player2) {
 }
 
 // Problem - 4
+
+function isSame(arr1, arr2) {
+  if (Array.isArray(arr1) !== true || Array.isArray(arr2) !== true) {
+    return "Invalid";
+  } else {
+    if (arr1.length !== arr2.length) {
+      return false;
+    }
+    for (let i = 0; i < arr1.length; i++) {
+      const val1 = arr1[i];
+      const val2 = arr2[i];
+      if (typeof val1 !== typeof val2) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
