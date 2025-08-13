d0c005baa97ac5d2df933e8915a2bed477e5e216
Antor-Chowdhury <antor1347@gmail.com>
2025-08-13 22:10:35 +0600

    fix issues with problem-3 solution

--- final.js
+++ final.js
@@ -26,11 +26,11 @@ function bestTeam(player1, player2) {
   if (typeof player1 !== "object" || typeof player2 !== "object") {
     return "Invalid";
   } else {
-    let team1 = player1.foul + player1.cardY + player1.cardR;
-    let team2 = player2.foul + player2.cardY + player2.cardR;
+    const team1 = player1.foul + player1.cardY + player1.cardR;
+    const team2 = player2.foul + player2.cardY + player2.cardR;
     if (team1 < team2) {
       return player1.name;
-    } else if ((team1 = team2)) {
+    } else if (team1 === team2) {
       return "Tie";
     } else {
       return player2.name;
